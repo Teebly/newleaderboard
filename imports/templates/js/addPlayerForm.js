7cf72a0226257e4ce6b6ef02a255e559a28c6f4c
Vincent Zocca <vincent@teebly.co>
2018-10-30 21:53:13 +0100

    30.10.18 review from Phil please

--- imports/templates/js/addPlayerForm.js
+++ imports/templates/js/addPlayerForm.js
@@ -3,45 +3,47 @@ const disciplines = ["Football", "Ice Hockey", "Athletics"];
 Template.addPlayerForm.onRendered(function() {
   $(document).foundation();
 });
+
 Template.addPlayerForm.events({
-  "click .successButton": function(event) {
+  "click #submitPlayer": function(event) {
     event.preventDefault();
     let playerNameVar = $("[name=playerName]").val();
     let playerLastNameVar = $("[name=playerLastName]").val();
     let disciplineVar = $("[name=discipline] option:checked").val();
-    let error = [];
-    let exists = PlayersList.findOne({
-      firstname: playerNameVar,
-      lastname: playerLastNameVar
-    });
-    if (exists) error.push("Sorry that user already exists.");
-    if (playerNameVar.length < 6) {
-      error.push("Name is to short.");
-    }
-    if (playerLastNameVar.length < 6) {
-      error.push("Lastname is to short.");
-    }
-    if (playerNameVar.length > 25) {
-      error.push("Name is to long.");
-    }
-    if (playerLastNameVar.length > 25) {
-      error.push("Lastname is to long.");
-    }
-    if (!disciplines.includes(disciplineVar)) {
-      error.push("Choose a valid discipline.");
-    }
-    if (error.length) alert(error);
-    else {
-      Meteor.call(
-        "createPlayer",
-        playerNameVar,
-        playerLastNameVar,
-        disciplineVar
-      );
-
-      alert(playerNameVar + playerLastNameVar);
-      playerNameVar = "";
-      playerLastNameVar = "";
-    }
+    console.log(playerNameVar, playerLastNameVar, disciplineVar);
+    // let error = [];
+    // let exists = PlayersList.findOne({
+    //   firstname: playerNameVar,
+    //   lastname: playerLastNameVar
+    // });
+    // if (exists) error.push("Sorry that user already exists.");
+    // if (playerNameVar.length < 6) {
+    //   error.push("Name is to short.");
+    // }
+    // if (playerLastNameVar.length < 6) {
+    //   error.push("Lastname is to short.");
+    // }
+    // if (playerNameVar.length > 25) {
+    //   error.push("Name is to long.");
+    // }
+    // if (playerLastNameVar.length > 25) {
+    //   error.push("Lastname is to long.");
+    // }
+    // if (!disciplines.includes(disciplineVar)) {
+    //   error.push("Choose a valid discipline.");
+    // }
+    // if (error.length) alert(error);
+    // else {
+    //   Meteor.call(
+    //     "createPlayer",
+    //     playerNameVar,
+    //     playerLastNameVar,
+    //     disciplineVar
+    //   );
+    //   // not setting fields back to empty
+    //   // alert(playerNameVar + playerLastNameVar);
+    //   playerNameVar = "";
+    //   playerLastNameVar = "";
+    // }
   }
 });
--- imports/templates/js/editPlayerForm.js
+++ imports/templates/js/editPlayerForm.js
@@ -1,53 +1,67 @@
+const disciplines = ["Football", "Ice Hockey", "Athletics"];
+
 Template.editPlayerForm.onRendered(function() {
   $(document).foundation();
 
   // console.log(this.data.Player.discipline);
   // console.log(this.data.Player._id);
 
-  $("").on("mouseover", function() {
-    alert("Hello World");
-    // $("#discipline option[value='" + this.data.Player.discipline + "']").prop(
-    //   "selected",
-    //   true
-    // );
-  });
+  //   $("").on("mouseover", function() {
+  //     alert("Hello World");
+  //   $("#discipline option[value='" + this.data.Player.discipline + "']").prop(
+  //     "selected",
+  //     true
+  //   );
+  //   //   });
 });
 
-// Template.editPlayerForm.events({
-//   "click #editPlayer": function(event) {
-//     event.preventDefault();
-//     let playerNameVar = event.target.playerName.value;
-//     let playerLastNameVar = event.target.playerLastName.value;
-//     let disciplineVar = event.target.discipline.value;
-//     let error = [];
-//     let exists = PlayersList.findOne({
-//       firstname: playerNameVar,
-//       lastname: playerLastNameVar
-//     });
-//     if (exists) error.push("Sorry that user already exists.");
-//     if (playerNameVar.length < 6) {
-//       error.push("Name is to short.");
-//     }
-//     if (playerLastNameVar.length < 6) {
-//       error.push("Lastname is to short.");
-//     }
-//     if (playerNameVar.length > 25) {
-//       error.push("Name is to long.");
-//     }
-//     if (playerLastNameVar.length > 25) {
-//       error.push("Lastname is to long.");
-//     }
-//     if (!disciplines.includes(disciplineVar)) {
-//       error.push("Choose a valid discipline.");
-//     }
-//     if (error.length) alert(error);
-//     else {
-//       Meteor.call(
-//         "editPlayer",
-//         playerNameVar,
-//         playerLastNameVar,
-//         disciplineVar
-//       );
-//     }
-//   }
-// });
+Template.editPlayerForm.events({
+  "click #editPlayerButton": function(event) {
+    event.preventDefault();
+    let playerNameVar = $("input [name=playerName]").val();
+    // let playerNameVar = event.target.playerName.value;
+    // let playerLastNameVar = event.target.playerLastName.value;
+    // let disciplineVar = event.target.discipline.value;
+    // let playerNameVar = $("[name=playerName]").val();
+    // let playerLastNameVar = $("[name=playerLastName]").val();
+    // let disciplineVar = $("[name=discipline] option:checked").val();
+    // let playerNameVar = $(
+    //   "#playerName_Id value+'" + this.data.Player.firstname + "'"
+    // ).val();
+    let playerLastNameVar = $("[name=playerLastName]").val();
+    // let playerNameVar = this.data.Player.firstname;
+    // let playerLastNameVar = this.data.Player.lastname;
+    // let disciplineVar = this.data.Player.discipline;
+    console.log(playerNameVar);
+    // let error = [];
+    // let exists = PlayersList.findOne({
+    //   firstname: playerNameVar,
+    //   lastname: playerLastNameVar
+    // });
+    // if (exists) error.push("Sorry that user already exists.");
+    // if (playerNameVar.length < 6) {
+    //   error.push("Name is to short.");
+    // }
+    // if (playerLastNameVar.length < 6) {
+    //   error.push("Lastname is to short.");
+    // }
+    // if (playerNameVar.length > 25) {
+    //   error.push("Name is to long.");
+    // }
+    // if (playerLastNameVar.length > 25) {
+    //   error.push("Lastname is to long.");
+    // }
+    // if (!disciplines.includes(disciplineVar)) {
+    //   error.push("Choose a valid discipline.");
+    // }
+    // if (error.length) alert(error);
+    // else {
+    //   Meteor.call(
+    //     "editPlayer",
+    //     playerNameVar,
+    //     playerLastNameVar,
+    //     disciplineVar
+    //   );
+    // }
+  }
+});
