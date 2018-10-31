9857542ddaecc88e5a9f0c8016b442026d4aaea1
Vincent Zocca <vincent@teebly.co>
2018-10-31 14:02:43 +0100

    31.10.18 commit to get uptodate

--- client/main.js
+++ client/main.js
@@ -3,19 +3,12 @@ import { ReactiveVar } from "meteor/reactive-var";
 //import foundation
 import Foundation from "foundation-sites/dist/js/foundation.js";
 import "foundation-sites/dist/css/foundation.css";
-<<<<<<< HEAD
 //import fontawesome and icons
-=======
->>>>>>> master
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faMinus } from "@fortawesome/free-solid-svg-icons";
 import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
-<<<<<<< HEAD
 import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
-=======
-import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
->>>>>>> master
 
 import "/imports/main.html";
 import "/imports/styles/app.css";
@@ -54,126 +47,4 @@ dom.watch();
 
 if (Meteor.isClient) {
   Meteor.subscribe("thePlayers");
-<<<<<<< HEAD
-=======
-  Template.leaderboard.helpers({
-    players: function() {
-      let currentUserId = Meteor.userId();
-      return PlayersList.find(
-        { createdBy: currentUserId },
-        { sort: { score: -1, firstname: 1 } }
-      );
-    }
-  });
-
-  Template.leaderboard.onRendered(function() {
-    $(document).foundation();
-  });
-
-  Template.PlayerCard.events({
-    "click .increment": function(event, instance) {
-      Meteor.call("updateScore", instance.data.Player._id, 5);
-    },
-    "click .decrement": function(event, instance) {
-      Meteor.call("updateScore", instance.data.Player._id, -5);
-    },
-    "click .remove": function(event, instance) {
-      Meteor.call("removePlayer", instance.data.Player._id);
-    }
-  });
-  Template.addPlayerForm.onRendered(function() {
-    $(document).foundation();
-  });
-  Template.addPlayerForm.events({
-    "click .successButton": function(event) {
-      event.preventDefault();
-      let playerNameVar = $("[name=playerName]").val();
-      let playerLastNameVar = $("[name=playerLastName]").val();
-      let disciplineVar = $("[name=discipline] option:checked").val();
-      let error = [];
-      let exists = PlayersList.findOne({
-        firstname: playerNameVar,
-        lastname: playerLastNameVar
-      });
-      if (exists) error.push("Sorry that user already exists.");
-      if (playerNameVar.length < 6) {
-        error.push("Name is to short.");
-      }
-      if (playerLastNameVar.length < 6) {
-        error.push("Lastname is to short.");
-      }
-      if (playerNameVar.length > 25) {
-        error.push("Name is to long.");
-      }
-      if (playerLastNameVar.length > 25) {
-        error.push("Lastname is to long.");
-      }
-      if (!disciplines.includes(disciplineVar)) {
-        error.push("Choose a valid discipline.");
-      }
-      if (error.length) alert(error);
-      else {
-        Meteor.call(
-          "createPlayer",
-          playerNameVar,
-          playerLastNameVar,
-          disciplineVar
-        );
-      }
-    }
-  });
-
-  Template.editPlayerForm.onRendered(function() {
-    $(document).foundation();
-  });
-
-  Template.editPlayerForm.events({
-    "click #editPlayer": function(event) {
-      event.preventDefault();
-      let playerNameVar = event.target.playerName.value;
-      let playerLastNameVar = event.target.playerLastName.value;
-      let disciplineVar = event.target.discipline.value;
-      let error = [];
-      let exists = PlayersList.findOne({
-        firstname: playerNameVar,
-        lastname: playerLastNameVar
-      });
-      if (exists) error.push("Sorry that user already exists.");
-      if (playerNameVar.length < 6) {
-        error.push("Name is to short.");
-      }
-      if (playerLastNameVar.length < 6) {
-        error.push("Lastname is to short.");
-      }
-      if (playerNameVar.length > 25) {
-        error.push("Name is to long.");
-      }
-      if (playerLastNameVar.length > 25) {
-        error.push("Lastname is to long.");
-      }
-      if (!disciplines.includes(disciplineVar)) {
-        error.push("Choose a valid discipline.");
-      }
-      if (error.length) alert(error);
-      else {
-        Meteor.call(
-          "editPlayer",
-          playerNameVar,
-          playerLastNameVar,
-          disciplineVar
-        );
-      }
-    }
-  });
-  Template.removePlayerForm.onRendered(function() {
-    $(document).foundation();
-  });
-  Template.removePlayerForm.events({
-    "click #removePlayer": function(event) {
-      PlayersList.remove({
-        player: _id
-      });
-    }
-  });
->>>>>>> master
 }
