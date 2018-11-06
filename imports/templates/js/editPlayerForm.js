1b1486a7365fd6465968ee283679b7f3d3d4f382
philkoenig <phil@teebly.co>
2018-11-06 11:53:20 +0100

    Phils-changes (#9)
    * latest version, couple of tries commented out, most valuable ones left uncommented
    
    * styling for diffrent formats v1.0
    
    * small changes left and right, see comments in PR for details

--- imports/templates/js/editPlayerForm.js
+++ imports/templates/js/editPlayerForm.js
@@ -17,94 +17,85 @@ Template.editPlayerForm.onRendered(function() {
   //     //your code here
   //   });
 
-  // let editDisciplineVar = $(
-  //   "#discipline option[value='" + this.data.Player.discipline + "']"
-  // )
-  //   .prop("selected", true)
-  //   .val();
-  // console.log(editDisciplineVar);
+  let editDisciplineVar = $(
+    "#discipline option[value='" + this.data.Player.discipline + "']"
+  )
+    .prop("selected", true)
+    .val();
+  console.log(editDisciplineVar);
 
-  // $("select", ".editForm").each(function() {
-  //   console.log(this.value);
-  // });
-}),
-  Template.editPlayerForm.helpers({
-    selectedDiscipline() {
-      let editDisciplineVar = $(
-        "#discipline option[value='" + this.data.Player.discipline + "']"
-      )
-        .prop("selected", true)
-        .val();
-      return console.log(editDisciplineVar);
-    }
-  }),
-  Template.editPlayerForm.events({
-    "submit .editForm": function(event) {
-      event.preventDefault();
-
-      console.log("On Submit", event);
-      let editPlayerFormId = event.currentTarget.id;
-      console.log(editPlayerFormId);
-      let editPlayerFirstNameVar = $(event.currentTarget)
-        .find("input#editPlayerFirstName")
-        .val();
-
-      let editPlayerLastNameVar = $(event.currentTarget)
-        .find("input#editPlayerLastName")
-        .val();
-
-      let editDisciplineVar = $(event.currentTarget)
-        .find("select#discipline")
-        .find(":selected")
-        .val();
-
-      console.log(
-        editPlayerFirstNameVar,
-        editPlayerLastNameVar,
-        editDisciplineVar
-      );
-    }
-    // "click #editPlayerButton": function(event) {
-    //   event.preventDefault();
-    //   console.log("event", event);
-    //   // tmpEdit = $("form#editPlayer").this.attr("playerId");
-    //   tmpFirst = $("input#editPlayerFirstName");
-    //   let editPlayerFirstNameVar = tmpFirst.val();
-    //   tmpLast = $("input#editPlayerLastName");
-    //   let editPlayerLastNameVar = tmpLast.val();
-    //   let playerId = tmpFirst.attr("playerId");
-    //   console.log(editPlayerFirstNameVar, editPlayerLastNameVar, playerId);
-    // console.log(tmpEdit);
-    // let error = [];
-    // let exists = PlayersList.findOne({
-    //   firstname: playerNameVar,
-    //   lastname: playerLastNameVar
-    // });
-    // if (exists) error.push("Sorry that user already exists.");
-    // if (playerNameVar.length < 6) {
-    //   error.push("Name is to short.");
-    // }
-    // if (playerLastNameVar.length < 6) {
-    //   error.push("Lastname is to short.");
-    // }
-    // if (playerNameVar.length > 25) {
-    //   error.push("Name is to long.");
-    // }
-    // if (playerLastNameVar.length > 25) {
-    //   error.push("Lastname is to long.");
-    // }
-    // if (!disciplines.includes(disciplineVar)) {
-    //   error.push("Choose a valid discipline.");
-    // }
-    // if (error.length) alert(error);
-    // else {
-    //   Meteor.call(
-    //     "editPlayer",
-    //     playerNameVar,
-    //     playerLastNameVar,
-    //     disciplineVar,
-    //     playerId
-    //   );
-    // }
-    //}
+  $("select", ".editForm").each(function() {
+    console.log(this.value);
   });
+});
+
+Template.editPlayerForm.events({
+  "submit .editForm": function(event) {
+    event.preventDefault();
+
+    console.log("On Submit", event);
+    let editPlayerFormId = event.currentTarget.id;
+    console.log(editPlayerFormId);
+    let editPlayerFirstNameVar = $(event.currentTarget)
+      .find("input#editPlayerFirstName")
+      .val();
+
+    let editPlayerLastNameVar = $(event.currentTarget)
+      .find("input#editPlayerLastName")
+      .val();
+
+    let editDisciplineVar = $(event.currentTarget)
+      .find("select#discipline")
+      .find(":selected")
+      .val();
+
+    console.log(
+      editPlayerFirstNameVar,
+      editPlayerLastNameVar,
+      editDisciplineVar
+    );
+  }
+  // "click #editPlayerButton": function(event) {
+  //   event.preventDefault();
+  //   console.log("event", event);
+  //   // tmpEdit = $("form#editPlayer").this.attr("playerId");
+  //   tmpFirst = $("input#editPlayerFirstName");
+  //   let editPlayerFirstNameVar = tmpFirst.val();
+  //   tmpLast = $("input#editPlayerLastName");
+  //   let editPlayerLastNameVar = tmpLast.val();
+  //   let playerId = tmpFirst.attr("playerId");
+  //   console.log(editPlayerFirstNameVar, editPlayerLastNameVar, playerId);
+  // console.log(tmpEdit);
+  // let error = [];
+  // let exists = PlayersList.findOne({
+  //   firstname: playerNameVar,
+  //   lastname: playerLastNameVar
+  // });
+  // if (exists) error.push("Sorry that user already exists.");
+  // if (playerNameVar.length < 6) {
+  //   error.push("Name is to short.");
+  // }
+  // if (playerLastNameVar.length < 6) {
+  //   error.push("Lastname is to short.");
+  // }
+  // if (playerNameVar.length > 25) {
+  //   error.push("Name is to long.");
+  // }
+  // if (playerLastNameVar.length > 25) {
+  //   error.push("Lastname is to long.");
+  // }
+  // if (!disciplines.includes(disciplineVar)) {
+  //   error.push("Choose a valid discipline.");
+  // }
+  // if (error.length) alert(error);
+  // else {
+  //   Meteor.call(
+  //     "editPlayer",
+  //     playerNameVar,
+  //     playerLastNameVar,
+  //     disciplineVar,
+  //     playerId
+  //   );
+  // }
+  //}
+});
